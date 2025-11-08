ec1ed19f1cc6d8a47ee11281b024f7057c56b1dc
Manuela Calderaro <mcalderaro12@gmail.com>
2025-11-07 22:32:18 -0300

    Add files via upload

---
+++ typography.ts
@@ -0,0 +1,13 @@
+export const Fonts = {
+  LatoBold: 'Lato-Bold',
+  LatoRegular: 'Lato-Regular',
+  RobotoBold: 'Roboto-Bold',
+};
+
+export const FontSizes = {
+  title: 24,
+  label: 16,
+  body: 14,
+  button: 14,
+  header: 18,
+};
